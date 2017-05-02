1a18b690506e8d36d47c7ac5ffb3611ac07403d7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-02 09:53:44 -0400

    Carlingwood 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -2,4 +2,7 @@ function init() {
     $(".btn-nav").hover(function() {
         $(".dd_animated").css({"display": "block"});    
     });
+    
+    var promo_list = getPromotionsList();
+    console.log(promo_list);
 }
