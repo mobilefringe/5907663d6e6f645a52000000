a12fceff636b96bb6ee0602f9f2dfba4843f413e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-03 15:37:43 -0400

    Carlingwood 1.0  /application.js

--- application.js
+++ application.js
@@ -191,7 +191,7 @@ function renderHomeHours(container, template, collection){
         if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
             var open_time = moment(val.open_time).tz(getPropertyTimeZone());
             var close_time = moment(val.close_time).tz(getPropertyTimeZone());
-            val.h = open_time.format("h:mm A") + " - " + close_time.format("h:mm A");
+            val.h = open_time.format("h:mma") + " - " + close_time.format("h:mma");
         } else {
             val.h = "Closed";
         }
