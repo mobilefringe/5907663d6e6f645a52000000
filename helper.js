70fa931704089daf1afd787bd655a1aac7796798
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-03 15:32:47 -0400

    Carlingwood 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -61,7 +61,6 @@ function init() {
     renderPromotions("#side_promo_container", "#side_promo_template", side_promos);
     
     var todays_hours = getTodaysHours();
-    console.log(todays_hours);
     renderHomeHours("#side_hours_container", "#side_hours_template", todays_hours);
     
     var prop_details = getPropertyDetails();
