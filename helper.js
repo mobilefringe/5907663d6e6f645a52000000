360eb934c5dce32e5cf1b3f3a5ff5b0f5b975500
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-03 12:41:19 -0400

    Carlingwood 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -1,8 +1,22 @@
 function init() {
-    // $(".btn-nav").hover(function() {
-    //     $(".dd_animated").css({"display": "block"});    
-    // });
-    
+    function animate_dropdown(id){
+        if ($("#"+id).is(":visible")){
+            $("#"+id).slideUp();
+        } else {
+            if ($(".dd_animated").is(":visible")){
+                $(".dd_animated").slideUp();
+                $(".btn-nav").css({"background-color":"#da5840", "font-family":"verdana"});
+               
+                $("#btn-"+id).css({"background-color":"#a82911" ,"font-family":"verdana"});
+                 $("#"+id).slideDown();
+            } else {
+                
+                $("#btn-"+id).css({"background-color":"#a82911" ,"font-family":"verdana"});
+                $("#"+id).slideDown(); 
+            }
+        }
+    }
+
     var promo_list = getPromotionsList();
     var side_promos = promo_list.slice(0,3);
     renderPromotions("#side_promo_container", "#side_promo_template", side_promos);
