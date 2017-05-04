bdf439c09e8de736c6b82be479fcdddd7ba13536
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-04 09:40:09 -0400

    Carlingwood 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -55,7 +55,9 @@ function init() {
             alert("Please enter a valid email address. ")
         }
     }
-    
+}
+
+function show_content() {
     var promo_list = getPromotionsList();
     var side_promos = promo_list.slice(0,3);
     renderPromotions("#side_promo_container", "#side_promo_template", side_promos);
