0a9853c6944bee6efe9d23d2e088da5e0207423a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-11 16:14:20 -0400

    Carlingwood 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -50,6 +50,17 @@ function init() {
             alert("Please enter a valid email address. ")
         }
     }
+    
+    function getUrlVars(){
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++){
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
 }
 
 function show_content() {
