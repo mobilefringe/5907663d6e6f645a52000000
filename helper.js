795b54d5d0463b352d794438b690e6e47fc89095
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-05-12 09:54:30 -0400

    Carlingwood 1.0  /helper.js

--- helper.js
+++ helper.js
@@ -25,6 +25,30 @@ function init() {
     //         }
     //     }
     // }
+    function subscribe_email(){ 
+        if (isValidEmailAddress($("#subscribe_email").val())){            
+            var data = {}
+            var contest = {}
+            contest["email"] = $("#subscribe_email").val();
+            contest["newsletter"] = true;
+            contest["property_id"] = 51;
+            data["contest"] = contest
+            data["notice"] = "false"
+            $.ajax({
+                url: "/newsletter_no_captcha",
+                type: "POST",
+                data: data,
+                success: function(response){                    
+                    alert("Thank you for signing up.");
+                },
+                error: function(xhr, ajaxOptions, thrownError){
+                    alert("Please try again later.");
+                }
+            })    
+        } else {
+            alert("Please enter a valid email address. ")
+        }
+    }
 
     function side_subscribe_email(){ 
         if (isValidEmailAddress($("#side_subscribe_email").val())){            
